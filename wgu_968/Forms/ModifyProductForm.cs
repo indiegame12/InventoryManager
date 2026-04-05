0ec90f9bc6c8a8c3f2b35d3a212241d3b058260b
indiegame12 <131648634+indiegame12@users.noreply.github.com>
2026-04-05 11:47:09 -0400

    Fixed crashing bug

--- wgu_968/Forms/ModifyProductForm.cs
+++ wgu_968/Forms/ModifyProductForm.cs
@@ -128,9 +128,16 @@ namespace wgu_968.Forms
             if (inputPartTextField.Text != "")
             {
                 int partId;
+
+                if (!int.TryParse(inputPartTextField.Text, out partId))
+                {
+                    MessageBox.Show("Only numbers can be entered into the search. Example 8");
+                    
+                    return;
+                }
+
                 partId = Convert.ToInt32(inputPartTextField.Text);
                 Part part = Inventory.lookupPart(partId);
-
                 if (part != null)
                 {
                     dgvModifyParts.DataSource = new List<Part> { part };
@@ -139,13 +146,6 @@ namespace wgu_968.Forms
                 {
                     MessageBox.Show("Part does not exist");
                 }
-                if (!int.TryParse(inputPartTextField.Text, out partId))
-                {
-                    MessageBox.Show("Only numbers can be entered into the search. Example 8");
-       
-                    return;
-                }
-             
             }
             else
             {
